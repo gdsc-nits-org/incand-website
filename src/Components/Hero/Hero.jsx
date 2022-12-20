c81044b04379a5ac9d3d25e0155e572a1865eb8b
Vinayak Das <vinayakd909@gmail.com>
2022-12-20 16:16:03 +0530

    ADD: Background Animation Change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import heroStyles from "./Hero.module.scss"
+import HALO from 'vanta/dist/vanta.halo.min.js'
+import * as THREE from 'three'
 
 const Hero = () => {
+  const [vantaEffect, setVantaEffect] = useState(null)
+  const myRef = useRef(null)
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(HALO({
+        el: myRef.current,
+        backgroundColor: 0x0c0e1d,
+        THREE
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
   return (
-    <>
+    <div ref={myRef}>
         <div className={heroStyles.heroSection}>
             <div className={heroStyles.socialHandle}>
                 <div className={heroStyles.socialIcon}>
@@ -22,7 +38,7 @@ const Hero = () => {
               <img src="/logos/heroMainLogo.svg" alt="heroMainLogo" />
             </div>
         </div>
-    </>
+    </div>
   )
 }
 
