615a03ed9a4177761ba1edcd86a406d00d2b7314
Jyotishka Bhattacharjee <jyotishka2002@gmail.com>
2022-12-24 10:54:09 +0530

    added about us and about NIT content

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import aboutStyles from "./About.module.scss"
 
+import { aboutIncand, aboutUs } from "../websiteContent/content.js"
+
 const About = () => {
   return (
     <>
-        <div className={aboutStyles.aboutIncand}>
-            <div className={aboutStyles.aboutUs}>
-                <div className={aboutStyles.aboutHeading}>About Us</div>
-                <div className={aboutStyles.aboutDesc}>Lorem ipsum dolor sit amet consectetur. Tincidunt cras quis amet habitasse ut velit accumsan. Suspendisse fringilla condimentum id id felis venenatis amet congue. Lorem ipsum dolor sit amet consectetur. Tincidunt cras quis amet habitasse ut velit accumsan. Suspendisse fringilla condimentum id id felis venenatis amet congue.</div>
-            </div>
-            <div className={aboutStyles.aboutNIT}>
-                <div className={aboutStyles.aboutHeading}>About NIT Silchar</div>
-                <div className={aboutStyles.aboutDesc}>Lorem ipsum dolor sit amet consectetur. Tincidunt cras quis amet habitasse ut velit accumsan. Suspendisse fringilla condimentum id id felis venenatis amet congue. Lorem ipsum dolor sit amet consectetur. Tincidunt cras quis amet habitasse ut velit accumsan. Suspendisse fringilla condimentum id id felis venenatis amet congue.</div>
-            </div>
+      <div className={aboutStyles.aboutIncand}>
+        <div className={aboutStyles.aboutUs}>
+          <div className={aboutStyles.aboutHeading}>About Us</div>
+          <div className={aboutStyles.aboutDesc}>{aboutIncand}</div>
         </div>
+        <div className={aboutStyles.aboutNIT}>
+          <div className={aboutStyles.aboutHeading}>About NIT Silchar</div>
+          <div className={aboutStyles.aboutDesc}>{aboutUs}</div>
+        </div>
+      </div>
     </>
   )
 }
