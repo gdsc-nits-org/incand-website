08a350c2448664b3b9aa16afe8f389a819718d69
masoom <masoomsahu20@gmail.com>
2022-12-28 12:31:28 +0530

    sponsors_contactus

--- src/Pages/Sponsors/Sponsors.jsx
+++ src/Pages/Sponsors/Sponsors.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import style from "./Sponsors.module.scss";
+
 
 const Sponsors = () => {
   return (
-    <div>Sponsors</div>
+    <div>
+
+      {/* *********************       CONTACT US- STARTS      *******************/}
+          <div className={style.contact}>
+          <div className={style.contactus}>
+            CONTACT US
+          </div>
+           <div className={style.contactbox}>
+            <div className={style.left}></div>
+            <div className={style.right}>
+             <input type="text" className= {style.field} placeholder='Name'/>
+             <input type="email" className= {style.field} placeholder='Email'/>
+             <input type="text" className= {style.field} placeholder='Query'/>
+             <button >Send</button>
+
+
+            </div>
+           </div>
+          </div>
+
+     {/* *********************          CONTACT US- ENDS       *******************/}
+
+
+    </div>
   )
 }
 
