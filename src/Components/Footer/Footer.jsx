6ab1d1d76a77f0db2e5bd316eaf5b57467717d36
Jyotishka Bhattacharjee <jyotishka2002@gmail.com>
2022-12-30 00:59:08 +0530

    footer links added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,36 +11,49 @@ const Footer = (props) => {
 
       <div className={footerStyles.buttons}>
         <div className={footerStyles.btn1}>
-          <button className={footerStyles.btn1son}>Download Brochure</button>
+          <a href="https://drive.google.com/file/d/1wIqo4KKqjLbHMHFyT46_0gnIZbfuXWdV/view">
+            <button className={footerStyles.btn1son}>Download Brochure</button>
+          </a>
         </div>
         <div className={footerStyles.btn2}>
-          <button className={footerStyles.btn2son}>Become our Campus Ambassador</button>
+          <a href="https://forms.gle/LZY5VWCWhHFDU84X9">
+            <button className={footerStyles.btn2son}>Become our Campus Ambassador</button>
+          </a>
         </div>
       </div>
 
       <div className={footerStyles.contactUs}>
         <p className={footerStyles.contact}>Contact Us</p>
-        <p className={footerStyles.mail}> user123@incad.org</p>
+        <p className={footerStyles.mail}> incandescence@nits.ac.in</p>
       </div>
 
       <div className={footerStyles.footerSocials}>
-        <Icon className={footerStyles.social1} icon="lucide:instagram" color="#d62976" />
-        <Icon
+        <a href="https://www.instagram.com/incandescence_nitsilchar/">
+          <Icon className={footerStyles.social1} icon="lucide:instagram" color="#d62976" />
+        </a>
+
+        {/* <Icon
           className={footerStyles.social2}
           icon="ant-design:twitter-circle-filled"
           color="#1da1f2"
-        />
-        <Icon
-          className={footerStyles.social3}
-          icon="ri:linkedin-box-line"
-          color="#0a66c2"
-        />
-        <Icon
-          className={footerStyles.social4}
-          icon="ic:baseline-facebook"
-          color="#4267b2"
-        />
-        <Icon className={footerStyles.social5} icon="ph:youtube-logo-fill" color="red" />
+        /> */}
+        <a href="https://www.linkedin.com/company/incandescence23/">
+          <Icon
+            className={footerStyles.social3}
+            icon="ri:linkedin-box-line"
+            color="#0a66c2"
+          />
+        </a>
+
+        <a href="https://www.facebook.com/incandescence.nits/">
+          <Icon
+            className={footerStyles.social4}
+            icon="ic:baseline-facebook"
+            color="#4267b2"
+          />
+        </a>
+
+        {/* <Icon className={footerStyles.social5} icon="ph:youtube-logo-fill" color="red" /> */}
       </div>
     </div>
   );
