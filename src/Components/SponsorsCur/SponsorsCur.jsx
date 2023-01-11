e4241a2050da8b2d2dcf4ec739fe37c23744994d
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-01-11 12:55:06 +0530

    background color added

--- src/Components/SponsorsCur/SponsorsCur.jsx
+++ src/Components/SponsorsCur/SponsorsCur.jsx
@@ -9,40 +9,20 @@ const SponsorsCur = () => {
       <div className={styles.body}>
         <img src="/logos/fireLogo.svg" alt="fire logo " className={styles.imgs} />
         <div className={styles.gallery}>
-          <div className={styles.glass}>
-            <img
-              src="/logos/asus.png"
-              alt="sponosr "
-              className={`${styles.Spons} ${styles.img1}`}
-            />
+          <div className={`${styles.glass} ${styles.img1}`}>
+            <img src="/logos/asus.png" alt="sponosr " />
           </div>
-          <div className={styles.glass}>
-            <img
-              src="/logos/asus.png"
-              alt="sponosr "
-              className={`${styles.Spons} ${styles.img2}`}
-            />
+          <div className={`${styles.glass} ${styles.img2}`}>
+            <img src="/logos/asus.png" alt="sponosr " />
           </div>
-          <div className={styles.glass}>
-            <img
-              src="/logos/asus.png"
-              alt="sponosr "
-              className={`${styles.Spons} ${styles.img3}`}
-            />
+          <div className={`${styles.glass} ${styles.img3}`}>
+            <img src="/logos/asus.png" alt="sponosr " />
           </div>
-          <div className={styles.glass}>
-            <img
-              src="/logos/asus.png"
-              alt="sponosr "
-              className={`${styles.Spons} ${styles.img4}`}
-            />
+          <div className={`${styles.glass} ${styles.img4}`}>
+            <img src="/logos/asus.png" alt="sponosr " />
           </div>
-          <div className={styles.glass}>
-            <img
-              src="/logos/asus.png"
-              alt="sponosr "
-              className={`${styles.Spons} ${styles.img5}`}
-            />
+          <div className={`${styles.glass} ${styles.img5}`}>
+            <img src="/logos/asus.png" alt="sponosr " />
           </div>
         </div>
       </div>
