bfb19f4212511115c86c8fac72f64d4694cde2af
Aditya <kumar.31aditya@gmail.com>
2023-01-15 00:58:17 +0530

    added prev sponsors

--- src/Components/SponsorPrev/SponsorPrev.jsx
+++ src/Components/SponsorPrev/SponsorPrev.jsx
@@ -6,15 +6,128 @@ const SponsorPrev = () => {
       <p>Previous Sponsors</p>
       <div className={pstyle.parent}>
         <div className={pstyle.glass}>
-          <img src="/logos/asus.png" alt="previous sponsors" className={pstyle.Spons} />
+          <img
+            src="/logos/campusblog.png"
+            alt="previous sponsors"
+            className={pstyle.Spons}
+          />
+        </div>
+        <div className={pstyle.glass}>
+          <img
+            src="/logos/college.png"
+            alt="previous sponsors"
+            className={pstyle.Spons}
+          />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/dare.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/grabon.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/jaipur.png" alt="previous sponsors" className={pstyle.Spons} />
         </div>
       </div>
-      <div className={pstyle.parent2}>
+      <div className={pstyle.parent}>
         <div className={pstyle.glass}>
-          <img src="/logos/asus.png" alt="previous sponsors" className={pstyle.Spons} />
+          <img src="/logos/london.png" alt="previous sponsors" className={pstyle.Spons} />
         </div>
         <div className={pstyle.glass}>
-          <img src="/logos/asus.png" alt="previous sponsors" className={pstyle.Spons} />
+          <img
+            src="/logos/ohcampus.png"
+            alt="previous sponsors"
+            className={pstyle.Spons}
+          />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/oolta.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/peta.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/quick.jpg" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+      </div>
+      <div className={pstyle.parent}>
+        <div className={pstyle.glass}>
+          <img
+            src="/logos/winkles.png"
+            alt="previous sponsors"
+            className={pstyle.Spons}
+          />
+        </div>
+        <div className={pstyle.glass}>
+          <img
+            src="/logos/campustimes.png"
+            alt="previous sponsors"
+            className={pstyle.Spons}
+          />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/kayem.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/vidur.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/song.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+      </div>
+      <div className={pstyle.parent}>
+        <div className={pstyle.glass}>
+          <img src="/logos/red.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/festo.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/chiiz.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/tribe.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/north.jpg" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+      </div>
+      <div className={pstyle.parent}>
+        <div className={pstyle.glass}>
+          <img src="/logos/du.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img
+            src="/logos/zebronics.png"
+            alt="previous sponsors"
+            className={pstyle.Spons}
+          />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/wiley.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/notice.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/souled.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+      </div>
+      <div className={pstyle.parent}>
+        <div className={pstyle.glass}>
+          <img src="/logos/coreo.jpg" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/youth.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/ed.jpg" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/vidur.png" alt="previous sponsors" className={pstyle.Spons} />
+        </div>
+        <div className={pstyle.glass}>
+          <img src="/logos/tree.png" alt="previous sponsors" className={pstyle.Spons} />
         </div>
       </div>
     </div>
