d9ccce7fd9177b7208c01f5a017a5b30d29b2cc8
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-01-25 18:10:29 +0530

    fix:bugs removed

--- src/Components/Photos/ImageCard.jsx
+++ src/Components/Photos/ImageCard.jsx
@@ -1,7 +1,21 @@
 import "./slider.css";
-
+import { useState } from "react";
 const Card = ({ src }) => {
-  return <img className="image" src={src} draggable="false" alt="pictures" />;
+  const [active, setActive] = useState(false);
+  const handleClick = (e) => {
+    setActive((prev) => {
+      return !prev;
+    });
+  };
+  return (
+    <img
+      onClick={handleClick}
+      className={active === true ? "image image-clicked" : "image"}
+      src={src}
+      draggable="false"
+      alt="pictures"
+    />
+  );
 };
 
 export default Card;
--- src/Components/Photos/Slider.jsx
+++ src/Components/Photos/Slider.jsx
@@ -1,63 +1,67 @@
 import Card from "./ImageCard";
 import "./slider.css";
 import { images } from "./Images";
+import { useRef, useEffect } from "react";
 const Slider = () => {
-  const track = document.querySelector("#image-track");
-  if (track) {
-    const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
+  const container = useRef(null);
+  const track = useRef(null);
+  const handleOnDown = (e) => (track.current.dataset.mouseDownAt = e.clientX);
 
-    const handleOnUp = () => {
-      track.dataset.mouseDownAt = "0";
-      track.dataset.prevPercentage = track.dataset.percentage;
-    };
+  const handleOnUp = (e) => {
+    track.current.dataset.mouseDownAt = "0";
+    track.current.dataset.prevPercentage = track.current.dataset.percentage;
+  };
+  const handleOnMove = (e) => {
+    if (track.current.dataset.mouseDownAt === "0") return;
 
-    const handleOnMove = (e) => {
-      if (track.dataset.mouseDownAt === "0") return;
+    const mouseDelta = parseFloat(track.current.dataset.mouseDownAt) - e.clientX,
+      maxDelta = window.innerWidth / 2;
 
-      const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
+    const percentage = (mouseDelta / maxDelta) * -100,
+      nextPercentageUnconstrained =
+        parseFloat(track.current.dataset.prevPercentage) + percentage,
+      nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-      const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained =
-          parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    track.current.dataset.percentage = nextPercentage;
 
-      track.dataset.percentage = nextPercentage;
+    track.current.animate(
+      {
+        transform: `translate(${nextPercentage}%, -50%)`,
+      },
+      { duration: 1200, fill: "forwards" }
+    );
 
-      track.animate(
+    for (const image of track.current.getElementsByClassName("image")) {
+      image.animate(
         {
-          transform: `translate(${nextPercentage}%, -50%)`,
+          objectPosition: `${100 + nextPercentage}% center`,
         },
         { duration: 1200, fill: "forwards" }
       );
+    }
+  };
+  useEffect(() => {
+    if (container && container.current) {
+      container.current.onmousedown = (e) => handleOnDown(e);
 
-      for (const image of track.getElementsByClassName("image")) {
-        image.animate(
-          {
-            objectPosition: `${100 + nextPercentage}% center`,
-          },
-          { duration: 1200, fill: "forwards" }
-        );
-      }
-    };
-    window.onmousedown = (e) => handleOnDown(e);
+      container.current.ontouchstart = (e) => handleOnDown(e.touches[0]);
 
-    window.ontouchstart = (e) => handleOnDown(e.touches[0]);
+      container.current.onmouseup = (e) => handleOnUp(e);
 
-    window.onmouseup = (e) => handleOnUp(e);
+      container.current.ontouchend = (e) => handleOnUp(e.touches[0]);
 
-    window.ontouchend = (e) => handleOnUp(e.touches[0]);
+      container.current.onmousemove = (e) => handleOnMove(e);
 
-    window.onmousemove = (e) => handleOnMove(e);
-
-    window.ontouchmove = (e) => handleOnMove(e.touches[0]);
-  }
-
+      container.current.ontouchmove = (e) => handleOnMove(e.touches[0]);
+    }
+  }, []);
+  console.log(container);
+  console.log(track);
   return (
-    <div className="slider-container">
-      <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+    <div className="slider-container" ref={container}>
+      <div id="image-track" ref={track} data-mouse-down-at="0" data-prev-percentage="0">
         {images.map((image) => {
-          return <Card src={image} key={image.id} />;
+          return <Card src={image.src} key={image.id} />;
         })}
       </div>
     </div>
