689b26a70dde1a12504f399ce4f0954bbf9a0cb5
Aditya <kumar.31aditya@gmail.com>
2023-01-28 00:36:30 +0530

    FIX: Team Cards

--- src/Components/TeamCard/TeamCard4.jsx
+++ src/Components/TeamCard/TeamCard4.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { FaFacebook, FaLinkedin } from 'react-icons/fa'
-import { BsGithub } from 'react-icons/bs'
+import { FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { BsGithub } from 'react-icons/bs';
 import Data from '../../Data/Team4.json'
 import style from "./TeamCard.module.scss";
 const TeamCard = () => {
