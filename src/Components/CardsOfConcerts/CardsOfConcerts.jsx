d9da2d0887a208ab8e56a271cc78351e6114abac
byt3h3ad <adhirajd007@gmail.com>
2023-02-04 00:12:55 +0530

    [enhancement] added events and concert details, removed hard coded text

--- src/Components/CardsOfConcerts/CardsOfConcerts.jsx
+++ src/Components/CardsOfConcerts/CardsOfConcerts.jsx
@@ -5,25 +5,31 @@ import styles from "./CardsOfConcerts.module.scss";
 import platform1 from "/images/platform1.png";
 import platform2 from "/images/platform2.png";
 import platform3 from "/images/platform3.png";
+import data from "/src/Data/ConcertsData.json";
 
 const CardsOfConcerts = () => {
   return (
     <div className={styles.CardsOfConcerts}>
-      <div className={styles.Card_body}>
-        <h1 className={styles.Card_header}>Thunder March</h1>
-        <p className={styles.Card_subheader}>Someone aptly said “If heavy metal bands ruled the world, we had be a lot better off.” and we cannot headbang to it more!</p>
-        <p className={styles.Card_text}> Rock music consists of a number of virtuoso, aggressive, and strong styles. Propelled by the ferocious sounds of the electric guitar in distortion, heavy metal is one of those ear splitting styles that creates a rhapsody among its listeners, whether it be “Hallowed by thy name” or </p>
-      </div>
-      <div className={styles.Card_body_2}>
-        <h1 className={styles.Card_header_2}>“Heaven and Hell”</h1>
-        <div className={styles.Card_images}>
-            <img src={platform1} alt="" />
-            <img src={platform3} alt="" />
-        </div>
-        <div className={styles.Card_images}>
-            <img src={platform2} alt="" />
-        </div>
-      </div>
+      {data &&
+        data.map((data) => (
+          <>
+            <div className={styles.Card_body}>
+              <h1 className={styles.Card_header}>{data?.header}</h1>
+              <p className={styles.Card_subheader}>{data?.subheader}</p>
+              <p className={styles.Card_text}>{data?.text}</p>
+            </div>
+            <div className={styles.Card_body_2}>
+              <h1 className={styles.Card_header_2}>{data?.header2}</h1>
+              <div className={styles.Card_images}>
+                <img src={platform1} alt="" />
+                <img src={platform3} alt="" />
+              </div>
+              <div className={styles.Card_images}>
+                <img src={platform2} alt="" />
+              </div>
+            </div>
+          </>
+        ))}
     </div>
   )
 }
