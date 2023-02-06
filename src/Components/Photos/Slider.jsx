c990723b1ab1def75a983a6ed7a3389d144405ed
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2023-02-07 00:20:02 +0530

    MERGE: Gallery button

--- src/Components/Photos/Slider.jsx
+++ src/Components/Photos/Slider.jsx
@@ -1,13 +1,18 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { Icon } from "@iconify/react";
-import Card from "./ImageCard";
-import "./slider.css";
-import { images } from "./Images";
 
-const Slider = () => {
+import Card from "./ImageCard";
+
+import style from "./Slider.module.scss";
+
+const Slider = ({ images }) => {
+  const container = useRef(null);
+  const track = useRef(null);
+
   const sideScroll = (element, speed, distance, step) => {
     let scrollAmount = 0;
     const slideTimer = setInterval(() => {
+      // eslint-disable-next-line no-param-reassign
       element.scrollLeft += step;
       scrollAmount += Math.abs(step);
       if (scrollAmount >= distance) {
@@ -18,68 +23,118 @@ const Slider = () => {
 
   const contentWrapper = useRef(null);
 
-  // const Slider = () => {
-  //   const container = useRef(null);
-  //   const track = useRef(null);
-  //   const handleOnDown = (e) => (track.current.dataset.mouseDownAt = e.clientX);
+  const handleOnDown = (e) => {
+    track.current.dataset.mouseDownAt = e.clientX;
+  };
 
-  //   const handleOnUp = (e) => {
-  //     track.current.dataset.mouseDownAt = "0";
-  //     track.current.dataset.prevPercentage = track.current.dataset.percentage;
-  //   };
-  //   const handleOnMove = (e) => {
-  //     if (track.current.dataset.mouseDownAt === "0") return;
+  // eslint-disable-next-line no-unused-vars
+  const handleOnUp = () => {
+    track.current.dataset.mouseDownAt = "0";
+    track.current.dataset.prevPercentage = track.current.dataset.percentage;
+  };
 
-  //     const mouseDelta = parseFloat(track.current.dataset.mouseDownAt) - e.clientX,
-  //       maxDelta = window.innerWidth / 2;
+  const handleOnMove = (e) => {
+    if (track.current.dataset.mouseDownAt === "0") return;
 
-  //     const percentage = (mouseDelta / maxDelta) * -100,
-  //       nextPercentageUnconstrained =
-  //         parseFloat(track.current.dataset.prevPercentage) + percentage,
-  //       nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    const mouseDelta = parseFloat(track.current.dataset.mouseDownAt) - e.clientX;
 
-  //     track.current.dataset.percentage = nextPercentage;
+    const maxDelta = window.innerWidth / 2;
 
-  //     track.current.animate(
-  //       {
-  //         transform: `translate(${nextPercentage}%, -50%)`,
-  //       },
-  //       { duration: 1200, fill: "forwards" }
-  //     );
-  //     for (const image of track.current.getElementsByClassName("image")) {
-  //       image.animate(
-  //         {
-  //           objectPosition: `${100 + nextPercentage}% center`,
-  //         },
-  //         { duration: 1200, fill: "forwards" }
-  //       );
-  //     }
-  //   };
-  //   useEffect(() => {
-  //     if (container && container.current) {
-  //       container.current.onmousedown = (e) => handleOnDown(e);
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentageUnconstrained =
+      parseFloat(track.current.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-  //       container.current.ontouchstart = (e) => handleOnDown(e.touches[0]);
+    //   const handleOnUp = (e) => {
+    //     track.current.dataset.mouseDownAt = "0";
+    //     track.current.dataset.prevPercentage = track.current.dataset.percentage;
+    //   };
+    //   const handleOnMove = (e) => {
+    //     if (track.current.dataset.mouseDownAt === "0") return;
 
-  //       container.current.onmouseup = (e) => handleOnUp(e);
+    //     const mouseDelta = parseFloat(track.current.dataset.mouseDownAt) - e.clientX,
+    //       maxDelta = window.innerWidth / 2;
+    track.current.animate(
+      {
+        transform: `translate(${nextPercentage}%, -50%)`,
+      },
+      { duration: 1200, fill: "forwards" }
+    );
+    for (const image of track.current.getElementsByClassName("image")) {
+      image.animate(
+        {
+          objectPosition: `${100 + nextPercentage}% center`,
+        },
+        { duration: 1200, fill: "forwards" }
+      );
+    }
+  };
 
-  //       container.current.ontouchend = (e) => handleOnUp(e.touches[0]);
+  useEffect(() => {
+    if (container && container.current) {
+      container.current.onmousedown = (e) => handleOnDown(e);
 
-  //       container.current.onmousemove = (e) => handleOnMove(e);
+      //     const percentage = (mouseDelta / maxDelta) * -100,
+      //       nextPercentageUnconstrained =
+      //         parseFloat(track.current.dataset.prevPercentage) + percentage,
+      //       nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-  //       container.current.ontouchmove = (e) => handleOnMove(e.touches[0]);
-  //     }
-  //   }, []);
+      //     track.current.dataset.percentage = nextPercentage;
+
+      //     track.current.animate(
+      //       {
+      //         transform: `translate(${nextPercentage}%, -50%)`,
+      //       },
+      //       { duration: 1200, fill: "forwards" }
+      //     );
+      //     for (const image of track.current.getElementsByClassName("image")) {
+      //       image.animate(
+      //         {
+      //           objectPosition: `${100 + nextPercentage}% center`,
+      //         },
+      //         { duration: 1200, fill: "forwards" }
+      //       );
+      //     }
+      //   };
+      //   useEffect(() => {
+      //     if (container && container.current) {
+      //       container.current.onmousedown = (e) => handleOnDown(e);
+
+      //       container.current.ontouchstart = (e) => handleOnDown(e.touches[0]);
+
+      //       container.current.onmouseup = (e) => handleOnUp(e);
+
+      //       container.current.ontouchend = (e) => handleOnUp(e.touches[0]);
+
+      //       container.current.onmousemove = (e) => handleOnMove(e);
+
+      //       container.current.ontouchmove = (e) => handleOnMove(e.touches[0]);
+      //     }
+      //   }, []);
+
+      container.current.ontouchmove = (e) => handleOnMove(e.touches[0]);
+    }
+  }, []);
 
   return (
-    <div className="slider-container" ref={contentWrapper}>
-      <div id="image-track">
+    <div className={style["slider-container"]} ref={container}>
+      <div
+        id={style["image-track"]}
+        ref={track}
+        data-mouse-down-at="0"
+        data-prev-percentage="0"
+      >
         <button
           onClick={() => {
             sideScroll(contentWrapper.current, 25, 100, 10);
           }}
         >
-          <Icon icon="material-symbols:arrow-back-ios-new-rounded" color="white" width={24} height={24}/>
+          <Icon
+            icon="material-symbols:arrow-back-ios-new-rounded"
+            color="white"
+            width={24}
+            height={24}
+          />
         </button>
         {images.map((image) => {
           return <Card src={image.src} key={image.id} />;
@@ -89,7 +144,12 @@ const Slider = () => {
             sideScroll(contentWrapper.current, 25, 100, -10);
           }}
         >
-          <Icon icon="material-symbols:arrow-forward-ios-new-rounded" color="white" width={24} height={24}/>
+          <Icon
+            icon="material-symbols:arrow-forward-ios-new-rounded"
+            color="white"
+            width={24}
+            height={24}
+          />
         </button>
       </div>
     </div>
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,27 +1,29 @@
 import { SponsorsHero, Slider } from "../../Components";
+
+import images from "../../Data/Images.json";
+
 import style from "./Gallery.module.scss";
+
 import footerStyles from "../../Components/Footer/Footer.module.scss";
+
 const Gallery = () => {
   return (
-    <>
-      {/* <Navbar /> */}
-      <div className={style.main}>
-        <SponsorsHero name="Gallery" />
-        <div className={style.text}>
-          <p>Day 1</p>
-        </div>
-        <Slider />
-        <div className={style.text}>
-          <p>Day 2</p>
-        </div>
-        <Slider />
-        <div className={footerStyles.footer}>
-          <div className={footerStyles.footerLogo}>
-            <img src="/logos/footerLogo.svg" alt="footer logo" />
-          </div>
+    <div className={style.main}>
+      <SponsorsHero name="Gallery" />
+      <div className={style.text}>
+        <p>Day 1</p>
+      </div>
+      <Slider images={images} />
+      <div className={style.text}>
+        <p>Day 2</p>
+      </div>
+      <Slider images={images} />
+      <div className={footerStyles.footer}>
+        <div className={footerStyles.footerLogo}>
+          <img src="/logos/footerLogo.svg" alt="footer logo" />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
