07eb39f6da2db2f66200187674d16275b11304a3
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2023-02-08 00:44:43 +0530

    FIX: Proper import

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,5 +1,3 @@
-import dragon from "../../../public/logos/drag.svg";
-
 import { MainHero, Slider } from "../../Components";
 
 import gallery from "../../Data/Gallery.json";
@@ -18,11 +16,14 @@ const Gallery = () => {
             <p>{g.heading}</p>
           </div>
           <Slider images={g.images} />
-          <div className={style.text} style={{justifyContent: "flex-end" , margin : "1.5rem"}}>
-            <p style={{ fontSize: "1rem"}}>Drag for more...</p>{" "}
+          <div
+            className={style.text}
+            style={{ justifyContent: "flex-end", margin: "1.5rem" }}
+          >
+            <p style={{ fontSize: "1rem" }}>Drag for more...</p>{" "}
             <img
-              src={dragon}
-              style={{ height: "4rem", width: "8rem"}}
+              src="/public/logos/drag.svg"
+              style={{ height: "4rem", width: "8rem" }}
               alt="butterfly"
             />
           </div>
