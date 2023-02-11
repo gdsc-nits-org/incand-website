23f87ca4b5d136b8f1861b3d52ab72e0f73628df
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-02-11 14:39:33 +0530

    merchandise section updated

--- src/Components/Merchandise/Merchandise.jsx
+++ src/Components/Merchandise/Merchandise.jsx
@@ -17,17 +17,24 @@ const Merchandise = () => {
     swipeToSlide: true,
   };
   return (
-    <>
-      <div className={styles.sliders}>
-        <Slider {...settings1}>
-          <Slides src="https://res.cloudinary.com/resyfer/image/upload/v1676050617/Incand/thunder_march_23_tshirt_mockup_x84t1q.png" />
-          <Slides src="https://res.cloudinary.com/resyfer/image/upload/v1676050690/Incand/VFinB_h6vnmu.png" />
-        </Slider>
-      </div>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button1}>Order Now</button>
-      </div>
-    </>
+    <div className={styles.sliders}>
+      <Slider {...settings1}>
+        <Slides
+          src="https://res.cloudinary.com/resyfer/image/upload/v1676050617/Incand/thunder_march_23_tshirt_mockup_x84t1q.png"
+          form="https://docs.google.com/forms/d/e/1FAIpQLSdKdGaT1ziGi1sXDiR0L-4FyVc5N3LYHh-Mx48TwbQFFl4ZWg/viewform?fbclid=IwAR3O0AFpOwFvR3pKr0MtxtWGuH3GfyxcnKYow-TulLCOr2181mkdtPOEbDY"
+        />
+        <Slides
+          src="https://res.cloudinary.com/resyfer/image/upload/v1676050690/Incand/VFinB_h6vnmu.png"
+          form="https://docs.google.com/forms/d/e/1FAIpQLSdKdGaT1ziGi1sXDiR0L-4FyVc5N3LYHh-Mx48TwbQFFl4ZWg/viewform?fbclid=IwAR3O0AFpOwFvR3pKr0MtxtWGuH3GfyxcnKYow-TulLCOr2181mkdtPOEbDY"
+        />
+        <Slides
+          src="https://res.cloudinary.com/resyfer/image/upload/v1676094215/Incand/IndieFront_wy49d9.png"
+          show="true"
+          form2="https://forms.gle/EYWKWd1ks5i1dmb29"
+          form="https://forms.gle/rg4syUF6ngPQmKxt5"
+        />
+      </Slider>
+    </div>
   );
 };
 
--- src/Components/Merchandise/Slides.jsx
+++ src/Components/Merchandise/Slides.jsx
@@ -3,7 +3,21 @@ import styles from "./Merchandise.module.scss";
 const Slides = (props) => {
   return (
     <div className={styles.container}>
-      <img src={props.src} alt="merchandise" />
+      <div className={styles.imgContainer}>
+        <img src={props.src} alt="merchandise" />
+      </div>
+      <div className={styles.buttonContainer}>
+        <a className={styles.button1} href={props.form} target="_blank">
+          Order Now
+          {props.show && <p style={{ fontSize: "8px" }}>NITS Student</p>}
+        </a>
+        {props.show && (
+          <a className={styles.button1} href={props.form2} target="_blank">
+            Order Now
+            {props.show && <p style={{ fontSize: "8px" }}>Non-NIT & Alumni</p>}
+          </a>
+        )}
+      </div>
     </div>
   );
 };
